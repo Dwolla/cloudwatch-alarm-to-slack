cad05d0bfbbd0f3f568416ab64fe87ec11afe5c0
Kent Schlorff <kschlorff@dwolla.com>
2019-09-24 09:22:18 -0500

    Add ability to customize stackName and specify arns for already-existing roles

--- serverless.js
+++ serverless.js
@@ -1,12 +1,35 @@
 const { serverless } = require("skripts/config")
 
+const fallbackRole = {
+  iamRoleStatements: [
+    {
+      Effect: "Allow",
+      Action: ["logs:GetQueryResults"],
+      Resource: "*"
+    },
+    {
+      Effect: "Allow",
+      Action: ["logs:StartQuery"],
+      Resource:
+        "arn:aws:logs:${self:provider.region}:#{AWS::AccountId}:log-group:/aws/lambda/webhooks-*-lambda-${self:provider.stage}:*"
+    }
+  ]
+}
+
+role = process.env.ROLE_ARN ? { role: "${ env: ROLE_ARN }" } : fallbackRole
+
+stackName = process.env.STACK_NAME
+  ? { stackName: "${ env: STACK_NAME }-${ env: ENVIRONMENT }" }
+  : null
+
 module.exports = {
   ...serverless,
   custom: {
     ...serverless.custom,
     tags: {
       ...serverless.custom.tags,
-      Team: "growth",
+      Team: "${env:TEAM_NAME, 'n/a'}",
+      Project: "${env:PROJECT_NAME, 'n/a'}",
       DeployJobUrl: "${env:BUILD_URL, 'n/a'}",
       "org.label-schema.vcs-url": "${env:GIT_URL, 'n/a'}",
       "org.label-schema.vcs-ref": "${env:GIT_COMMIT, 'n/a'}"
@@ -15,23 +38,12 @@ module.exports = {
   plugins: [...serverless.plugins, "serverless-pseudo-parameters"],
   provider: {
     ...serverless.provider,
+    ...stackName,
     environment: {
       ...serverless.provider.environment,
       SLACK_WEBHOOK_URL: "${ env: SLACK_WEBHOOK_URL }"
     },
-    iamRoleStatements: [
-      {
-        Effect: "Allow",
-        Action: ["logs:GetQueryResults"],
-        Resource: "*"
-      },
-      {
-        Effect: "Allow",
-        Action: ["logs:StartQuery"],
-        Resource:
-          "arn:aws:logs:${self:provider.region}:#{AWS::AccountId}:log-group:/aws/lambda/webhooks-*-lambda-${self:provider.stage}:*"
-      }
-    ]
+    ...role
   },
   functions: {
     func: {
