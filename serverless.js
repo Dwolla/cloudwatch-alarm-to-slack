0d30163352ce717f076be8bcd57d514933ba4d99
Lauren Gorski <lgorski@dwolla.com>
2022-09-29 11:42:58 -0500

    upgrade node from 12 to 16.17.x

--- serverless.js
+++ serverless.js
@@ -1,22 +1,26 @@
-const { serverless } = require("skripts/config")
-
 module.exports = {
-  ...serverless,
+  cfnRole: process.env.SKRIPTS_CFN_ROLE || null,
   custom: {
-    ...serverless.custom,
     tags: {
-      ...serverless.custom.tags,
+      Creator: "serverless",
+      Environment: "${self:provider.stage}",
+      Project: "${self:service.name}",
       Team: "growth",
       DeployJobUrl: "${env:BUILD_URL, 'n/a'}",
       "org.label-schema.vcs-url": "${env:GIT_URL, 'n/a'}",
       "org.label-schema.vcs-ref": "${env:GIT_COMMIT, 'n/a'}"
     }
   },
-  plugins: [...serverless.plugins, "serverless-pseudo-parameters"],
+  frameworkVersion: ">=1.0.0 <3.0.0",
   provider: {
-    ...serverless.provider,
+    deploymentBucket: process.env.SKRIPTS_DEPLOYMENT_BUCKET
+      ? {
+          name: process.env.SKRIPTS_DEPLOYMENT_BUCKET,
+          serverSideEncryption: "AES256"
+        }
+      : null,
     environment: {
-      ...serverless.provider.environment,
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: 1,
       SLACK_WEBHOOK_URL: "${ env: SLACK_WEBHOOK_URL }"
     },
     iamRoleStatements: [
@@ -31,8 +35,32 @@ module.exports = {
         Resource:
           "arn:aws:logs:${self:provider.region}:#{AWS::AccountId}:log-group:/aws/lambda/webhooks-*-lambda-${self:provider.stage}:*"
       }
-    ]
+    ],
+    lambdaHashingVersion: "20201221",
+    logRetentionInDays: 365,
+    memorySize: 128,
+    name: "aws",
+    region: "us-west-2",
+    runtime: "nodejs16.x",
+    stackTags: "${self:custom.tags}",
+    stage: "${opt:stage, env:STAGE, env:ENVIRONMENT}",
+    tags: "${self:custom.tags}",
+    timeout: 10
   },
+  package: { individually: true },
+  plugins: [
+    "serverless-iam-roles-per-function",
+    "serverless-pseudo-parameters",
+    "serverless-webpack"
+  ],
+  service: "${file(./package.json):name}",
+  vpc:
+    process.env.SKRIPTS_VPC_SECURITY_GROUPS && process.env.SKRIPTS_VPC_SUBNETS
+      ? {
+          securityGroupIds: process.env.SKRIPTS_VPC_SECURITY_GROUPS.split(","),
+          subnetIds: process.env.SKRIPTS_VPC_SUBNETS.split(",")
+        }
+      : null,
   functions: {
     func: {
       handler: "src/handler.handle",
